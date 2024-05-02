645b305357ed17c3b5b260d506e2e1c21ef74bf3
Stewalsh04 <SW2154@cam.ac.uk>
2024-05-02 15:52:34 +0100

    Gave the script some options

--- fastq.bash
+++ fastq.bash
@@ -13,10 +13,42 @@
 #this code makes STAR aligned bigwig tracks and gives a "final.out" file that tells you the #reads and #uniquely mapped reads
 #NB I use kallisto for the DESeq analysis (aligns to transcriptome, whilst STAR aligns to genome)
 
-outdir_star=/mnt/home3/ahringer/sw2154/out/
+outdir=/mnt/home3/ahringer/sw2154/out/
 genome_chr=/mnt/home3/ahringer/sw2154/references/built_genomes/star/c.elegans.latest
 fastq_dir=/mnt/home3/ahringer/sw2154/data/
 star_index=/mnt/home3/ahringer/sw2154/references/built_genomes/star/c.elegans.latest
+THREADS=1
+
+options=$(getopt -o '' -l threads: -l path: -l id: -l holdinput -l -- "$@") || exit_with_bad_args
+
+eval set -- "$options"
+while true; do
+    case "$1" in
+        --threads)
+            shift
+            THREADS="$1"
+            ;;
+        --path)
+            shift
+            fastq_dir="$1"
+            ;;
+        --id)
+            shift
+            RUNID="$1"
+            ;;
+        --holdinput)
+           HOLDINPUT="true"
+           echo "Holding input fastq files in inputs folder"
+           ;;
+         --)
+            shift
+            break
+            ;;
+    esac
+    shift
+done
+
+analysis_out_dir=${outdir}/${RUNID}
 
 declare -A FILES
 
@@ -33,10 +65,15 @@ for base in "${!FILES[@]}"; do
 echo "${base}_L001_R1_001.fastq.gz"
 echo "${base}_L001_R2_001.fastq.gz"
  
+ mkdir ${analysis_out_dir}/${RUNID}/${base}/fastq
 
-STAR --readFilesCommand zcat --runThreadN 20 --genomeDir $star_index --readFilesIn ${base}_L001_R1_001.fastq.gz ${base}_L001_R2_001.fastq.gz --outFileNamePrefix $outdir_star --outSAMtype BAM SortedByCoordinate --outSAMattrIHstart 0 --outWigType wiggle --twopassMode Bas>
+ cd ${analysis_out_dir}/${RUNID}/${base}/fastq
+
+fastq ${base}_L001_R1_001.fastq.gz ${base}_L001_R2_001.fastq.gz
 
 done
+
+
 
 # ~/wigToBigWig_tool/wigToBigWig $outdir_star/$sample.Signal.UniqueMultiple.str1.out.wig $genome_chr $outdir_star/$sample.Signal.UniqueMultiple.str1.out.bw
 # ~/wigToBigWig_tool/wigToBigWig $outdir_star/$sample.Signal.UniqueMultiple.str2.out.wig $genome_chr $outdir_star/$sample.Signal.UniqueMultiple.str2.out.bw
